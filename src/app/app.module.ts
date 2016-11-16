d50f2165cb4e3d065c84c79d7b8617372f8a9de0
annasophie <annasophie.nagel@gmail.com>
2016-11-15 22:50:38 -0500

    Working on date picker and autofill inputs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Directive, OnInit, Output, EventEmitter, Input, TemplateRef } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { ModalModule } from 'ng2-bootstrap/components/modal';
-import { TypeaheadModule } from 'ng2-bootstrap/components/typeahead';
+import { DatepickerModule } from 'ng2-bootstrap/components/datepicker';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header.component';
 import { SloganComponent } from './slogan.component';
 import { OptionsComponent } from './options/options.component';
 
+
+
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     SloganComponent,
-    OptionsComponent
+    OptionsComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
     ModalModule,
-    TypeaheadModule
+    DatepickerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
