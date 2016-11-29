090f35edc927de3d9384b8efcac6e87a1b0bf2ec
Ivey Topaz <iveytopaz@gmail.com>
2016-11-29 18:11:32 -0500

    Add glyphicons

--- src/app/secondPage/advanced-options/advanced-options.component.ts
+++ src/app/secondPage/advanced-options/advanced-options.component.ts
@@ -3,6 +3,7 @@ import { ModalDirective } from '../../../../node_modules/ng2-bootstrap/component
 import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 import { SendCostDataService } from '../../send-cost-data.service';
 
+
 @Component({
   selector: 'app-advanced-options',
   templateUrl: './advanced-options.component.html',
